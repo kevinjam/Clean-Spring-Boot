d55fae18af094cba5de503cebcf6c3b8b10a73d7
kevin janvier <kevin.janvier5@gmail.com>
2021-11-23 23:13:22 +0300

    update config

---
+++ src/main/java/com/thinkdevs/hotel/HotelApplication.java
@@ -0,0 +1,15 @@
+package com.thinkdevs.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class HotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelApplication.class, args);
+	}
+
+}
